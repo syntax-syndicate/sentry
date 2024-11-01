2e04b94f8b38651ad91663030a350b82bb3a04ac
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-11-01 01:58:05 -0400

    Manually leading double slashes

--- static/app/bootstrap/initializeSdk.tsx
+++ static/app/bootstrap/initializeSdk.tsx
@@ -135,7 +135,13 @@ export function initializeSdk(config: Config) {
 
       if (event.transaction) {
         event.transaction = normalizeUrl(event.transaction, {forceCustomerDomain: true});
+
+        // Due to an unplesant interaction of the React Router 6 integration and our React Router 6 shims, some transaction names get prepended with a double slash. e.g., "//dashboard/:dashboardId/widget/:widgetIndex/edit/" Will hopefully be resolved in an upcoming version of the SDK. For now, manually cover this case by removing the first slash of two.
+        if (event.transaction.startsWith('//')) {
+          event.transaction = event.transaction.substring(1);
+        }
       }
+
       return event;
     },
 
