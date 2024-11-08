32ce2b168cc35fb776bef86afcce4d4f3bfcbf7a
Christinarlong <christina.long@sentry.io>
2024-11-08 11:33:02 -0800

    make api public

--- src/sentry/sentry_apps/api/endpoints/organization_sentry_apps.py
+++ src/sentry/sentry_apps/api/endpoints/organization_sentry_apps.py
@@ -1,3 +1,5 @@
+from drf_spectacular.utils import extend_schema
+from rest_framework import serializers
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -7,6 +9,7 @@ from sentry.api.base import control_silo_endpoint
 from sentry.api.bases.organization import ControlSiloOrganizationEndpoint
 from sentry.api.paginator import OffsetPaginator
 from sentry.api.serializers import serialize
+from sentry.apidocs.parameters import GlobalParams
 from sentry.constants import SentryAppStatus
 from sentry.organizations.services.organization import RpcOrganization
 from sentry.organizations.services.organization.model import RpcUserOrganizationContext
@@ -17,13 +20,29 @@ from sentry.sentry_apps.api.serializers.sentry_app import (
 from sentry.sentry_apps.models.sentry_app import SentryApp
 
 
+class OrganizationSentryQueryParamsSerializer(serializers.Serializer):
+    status = serializers.ChoiceField(
+        choices=SentryAppStatus.as_choices(),
+        help_text=(),
+    )
+
+
+@extend_schema(tags=["Integration"])
 @control_silo_endpoint
 class OrganizationSentryAppsEndpoint(ControlSiloOrganizationEndpoint):
     owner = ApiOwner.INTEGRATIONS
     publish_status = {
-        "GET": ApiPublishStatus.UNKNOWN,
+        "GET": ApiPublishStatus.PUBLIC,
     }
 
+    @extend_schema(
+        operation_id="Retrieve the custom integrations by an organization",
+        parameters=[GlobalParams.ORG_ID_OR_SLUG, OrganizationSentryQueryParamsSerializer],
+        request=None,
+        responses={
+            200: list[ResponseSentryAppSerializer],
+        },
+    )
     @add_integration_platform_metric_tag
     def get(
         self,
@@ -31,6 +50,9 @@ class OrganizationSentryAppsEndpoint(ControlSiloOrganizationEndpoint):
         organization_context: RpcUserOrganizationContext,
         organization: RpcOrganization,
     ) -> Response:
+        """
+        Get the custom integrations the organization has created that are not deleted.
+        """
         queryset = SentryApp.objects.filter(owner_id=organization.id, application__isnull=False)
 
         status = request.GET.get("status")
