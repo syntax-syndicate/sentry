5c0b19b3d1dc7e6fbcbf89a59ab6c075d7e3f8f9
Leander Rodrigues <leander.rodrigues@sentry.io>
2024-11-29 14:51:53 -0500

    🚧 Still pretty rough

--- static/app/views/issueDetails/streamline/eventMissing.tsx
+++ static/app/views/issueDetails/streamline/eventMissing.tsx
@@ -5,13 +5,22 @@ import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 
 export function EventMissing() {
-  return (
+  const isFilteredApplied = false;
+  return isFilteredApplied ? (
     <MissingSection type="muted" showIcon>
       {t('No events found matching the above filters. Some possible fixes:')}
       <ReasonList>
         <li>{t('Changing the environment filter')}</li>
         <li>{t('Increasing the date range')}</li>
         <li>{t('Adjusting your event query')}</li>
+      </ReasonList>
+    </MissingSection>
+  ) : (
+    <MissingSection type="muted" showIcon>
+      {t("Couldn't find this event")}
+      <ReasonList>
+        <li>{t('The events are still processing and are on their way')}</li>
+        <li>{t('The events have been deleted')}</li>
       </ReasonList>
     </MissingSection>
   );
